007dd57dd83578f5b76fdcd71727ac3ba4717ce1
Jai Patel <jpate661@my.centennialcollege.ca>
2023-11-01 15:52:13 -0400

    Update pair_program_backend.py

--- pair_program_backend.py
+++ pair_program_backend.py
@@ -266,10 +266,10 @@ solution=st.text_area("Solution")
 # Just add it after st.sidebar:
 q=st.sidebar.text_area("Question"),
 a=st.sidebar.text_area("Hello, I am your Coding Companion. How Can I help!!👋")
+i=st.chat_input("Say something")
 col1, col2 = st.columns(2)
 col1.write('Column 1')
 col2.write('Column 2')  
-
 # from code_editor import code_editor
 # response_dict = code_editor("")
 
