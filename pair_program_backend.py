e23c99ed5883a2258696dafbe80c281b75085d4b
Jai Patel <jpate661@my.centennialcollege.ca>
2023-11-01 16:40:48 -0400

    s

--- pair_program_backend.py
+++ pair_program_backend.py
@@ -270,7 +270,8 @@ with col2:
 
 # Just add it after st.sidebar:
 q=st.sidebar.header('Question'),
-st.sidebar.code('for i in range(8): foo()')
+with st.echo():
+    st.sidebar.code('for i in range(8): foo()')
 a=st.sidebar.text_area("Hello, I am your Coding Companion. How Can I help!!👋")
 
 b=st.button("Compile")
